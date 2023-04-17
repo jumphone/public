683a5a42570f2d849ac052abd1691b4ce34fb4a4
Zhang, Feng <15110700005@fudan.edu.cn>
2023-04-17 23:25:52 +0800

    Update cerebellum_seurat.R

--- cerebellum_seurat.R
+++ cerebellum_seurat.R
@@ -17,10 +17,30 @@ pbmc <- subset(pbmc, subset = nFeature_RNA>2000 & type=='nuclei')
 pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
 
 
+SCORE=pbmc[['RNA']]@data['SOX11',]+pbmc[['RNA']]@data['HNRNPH1',]+pbmc[['RNA']]@data['CTNNB1',]
+pbmc$score=SCORE
+FeaturePlot(pbmc, features=c('score'),order=TRUE,reduction='umap')
+
+FeaturePlot(pbmc, slot='counts', features=c('HNRNPH1','SOX11','CTNNB1'),order=TRUE,reduction='umap')
+FeaturePlot(pbmc, slot='data',features=c('HNRNPH1','SOX11','CTNNB1'),order=TRUE,reduction='umap')
+
 source('https://gitee.com/jumphone/VISA/raw/master/VISA.R')
 EXP=rank(pbmc[['RNA']]@data['HNRNPH1',])
 COL=visa.vcol(EXP,c(min(EXP),quantile(EXP,0.95),max(EXP)),c('grey90','grey90','indianred1'))
 plot(pbmc@reductions$umap@cell.embeddings[order(EXP),], col=COL[order(EXP)], cex=0.5, pch=16)
+
+EXP=rank(pbmc[['RNA']]@data['SOX11',])
+COL=visa.vcol(EXP,c(min(EXP),quantile(EXP,0.95),max(EXP)),c('grey90','grey90','indianred1'))
+plot(pbmc@reductions$umap@cell.embeddings[order(EXP),], col=COL[order(EXP)], cex=0.5, pch=16)
+
+EXP=rank(pbmc[['RNA']]@data['CTNNB1',])
+COL=visa.vcol(EXP,c(min(EXP),quantile(EXP,0.95),max(EXP)),c('grey90','grey90','indianred1'))
+plot(pbmc@reductions$umap@cell.embeddings[order(EXP),], col=COL[order(EXP)], cex=0.5, pch=16)
+
+EXP=rank(pbmc$score)
+COL=visa.vcol(EXP,c(min(EXP),quantile(EXP,0.95),max(EXP)),c('grey90','grey90','indianred1'))
+plot(pbmc@reductions$umap@cell.embeddings[order(EXP),], col=COL[order(EXP)], cex=0.5, pch=16)
+
 
 DimPlot(pbmc, group.by='age',label=TRUE)+NoLegend()
 DimPlot(pbmc, group.by='figure_clusters',label=TRUE)+NoLegend()
