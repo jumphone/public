99a928db51ccfcba05e38457ef756fa169c2e030
Zhang, Feng <15110700005@fudan.edu.cn>
2023-04-20 10:18:05 +0800

    Update step06_statCellFrag.R

--- scATAC_lilab/step06_statCellFrag.R
+++ scATAC_lilab/step06_statCellFrag.R
@@ -2,7 +2,9 @@
 
 a=read.table('ALL_BC_PAIR.bam.bedpe.changed.bed.cell_stat.txt')
 
+nFrag=a[,2]
+
 pdf('cell_stat.pdf',width=5,height=5)
-plot(a[,2][order(-a[,2])],col='grey50',pch=16,cex=1)
-plot(density(a[,2]),type='h',lwd=2,col='grey70')
+plot(sort(nFrag,decreasing=TRUE),col='grey50',pch=16,cex=1)
+plot(density(nFrag),type='h',lwd=2,col='grey70')
 dev.off()
