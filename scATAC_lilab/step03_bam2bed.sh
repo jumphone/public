82dc28d6ead8b56b7b4f0eca4834514a93d2b5cb
Zhang, Feng <15110700005@fudan.edu.cn>
2023-04-20 10:52:59 +0800

    Update step03_bam2bed.sh

--- scATAC_lilab/step03_bam2bed.sh
+++ scATAC_lilab/step03_bam2bed.sh
@@ -1,6 +1,7 @@
 
 
-samtools sort -n Mapping/ALL_BC.pe.q10.sort.bam --threads 20 -o ALL_BC_PAIR.bam
+#samtools sort -n Mapping/ALL_BC.pe.q10.sort.bam --threads 20 -o ALL_BC_PAIR.bam
+samtools sort -n Mapping/ALL_BC.bam --threads 40 -o ALL_BC_PAIR.bam
 
 INPUT=ALL_BC_PAIR.bam
 bedtools bamtobed  -bedpe -i $INPUT  > $INPUT\.bedpe
