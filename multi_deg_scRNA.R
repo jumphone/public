9b49a65a8b9ada194fb13772973c84d016ec75d1
Zhang, Feng <15110700005@fudan.edu.cn>
2024-03-26 13:37:48 +0800

    Update multi_deg_scRNA.R

--- multi_deg_scRNA.R
+++ multi_deg_scRNA.R
@@ -5,7 +5,7 @@ library(Libra)
 
 SEED=123 # We have tested other seed and got similar results.
 pbmc=readRDS('../Cerebellum/Fetal_cerebellum_final_cluster_1.rds')
-#pbmc$cell_type=rep('All',ncol(pbmc))
+pbmc$cell_type=rep('All',ncol(pbmc))
 set.seed(SEED)
 pbmc$replicate=sample(c('S1','S2','S3','S4','S5'),ncol(pbmc),replace=T)
 pbmc$label=rep('nonTCP',length(pbmc$cell_type))
