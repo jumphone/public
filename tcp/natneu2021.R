87b752ed51dfc6e5cdee8c0da36e9d4620721db0
Zhang, Feng <15110700005@fudan.edu.cn>
2024-03-28 12:11:09 +0800

    Update natneu2021.R

--- tcp/natneu2021.R
+++ tcp/natneu2021.R
@@ -26,18 +26,35 @@ library(Seurat)
 natneu_2021=readRDS('/newdisk/database/TCP/RDS/NatNeu2021_seurat.rds') # get from UCSC single-cell database
 natneu_2021_counts=natneu_2021[['RNA']]@counts
 natneu_2021_meta=natneu_2021@meta.data
-natneu_2021_new <- CreateSeuratObject(counts = natneu_2021_counts, meta.data =natneu_2021_meta, project = "natneu_2021", min.cells = 0, min.features = 0)
+natneu_2021_umap=natneu_2021@reductions$umap@cell.embeddings
 
 saveRDS(natneu_2021_meta,'/newdisk/database/TCP/RDS/NatNeu2021_meta.rds')
 saveRDS(natneu_2021_counts,'/newdisk/database/TCP/RDS/NatNeu2021_counts.rds')
+saveRDS(natneu_2021_umap,'/newdisk/database/TCP/RDS/NatNeu2021_umap.rds')
 
 ##################
 
-natneu_2021_counts=readRDS('/newdisk/database/TCP/RDS/NatNeu2021_meta.rds')
-natneu_2021_meta=readRDS('/newdisk/database/TCP/RDS/NatNeu2021_counts.rds')
+
+#####################
+# Load data
+library(Seurat)
+
+natneu_2021_counts=readRDS('/newdisk/database/TCP/RDS/NatNeu2021_counts.rds')
+natneu_2021_meta=readRDS('/newdisk/database/TCP/RDS/NatNeu2021_meta.rds')
+natneu_2021_umap=readRDS('/newdisk/database/TCP/RDS/NatNeu2021_umap.rds')
+
+####################
+# Preprocess
 natneu_2021 <- CreateSeuratObject(counts = natneu_2021_counts, meta.data =natneu_2021_meta, project = "natneu_2021", min.cells = 0, min.features = 0)
+natneu_2021 <- NormalizeData(natneu_2021, normalization.method = "LogNormalize", scale.factor = 10000)
+natneu_2021 <- FindVariableFeatures(natneu_2021, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(natneu_2021)
+natneu_2021 <- ScaleData(natneu_2021, features = all.genes)
+natneu_2021 <- RunPCA(natneu_2021, features = VariableFeatures(object = natneu_2021))
+natneu_2021 <- RunUMAP(natneu_2021, dims = 1:10)
+natneu_2021@reductions$umap@cell.embeddings=natneu_2021_umap
 
-
+##########################
 # Quality control
 natneu_2021_nuclei=subset(natneu_2021,subset = nFeature_RNA > 500 & nFeature_RNA < 3000, cells=colnames(natneu_2021)[which(natneu_2021$type %in% c('nuclei','nulcei'))])
 
