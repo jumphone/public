74452abdb8b09e4a5b4911ca6cb0f6e7887673b0
Zhang, Feng <15110700005@fudan.edu.cn>
2024-04-03 12:37:47 +0800

    Update changeENSG.R

--- tcp/changeENSG.R
+++ tcp/changeENSG.R
@@ -7,29 +7,11 @@ DATA=ori.pbmc[['RNA']]@data
 library(clusterProfiler)  
 library(org.Hs.eg.db)
 
+ENSG=rownames(DATA)
+SYMBOL=mapIds(org.Hs.eg.db, ENSG, column="SYMBOL", keytype="ENSEMBL", multiVals="first")
 
-ensg_to_symbol <- toTable(org.Hs.egENSEMBL)  
-  
-# 只保留你数据中存在的ENSG号  
-ensg_in_data <- rownames(DATA)  
-ensg_to_symbol <- ensg_to_symbol[ensg_to_symbol$ensembl_id %in% ensg_in_data,]  
-  
-# 确保所有的ENSG号都能找到对应的基因符号  
-# 如果有找不到的，你可能需要手动处理或排除这些ENSG号  
-missing_ensg <- setdiff(ensg_in_data, ensg_to_symbol$ensembl_id)  
+USED=which(!is.na(SYMBOL))
 
-
-###############################
-
-DATA=DATA[which(!rownames(DATA) %in% missing_ensg),]
-
-ensg_to_symbol <- toTable(org.Hs.egENSEMBL)  
-ensg_in_data <- rownames(DATA)  
-ensg_to_symbol <- ensg_to_symbol[ensg_to_symbol$ensembl_id %in% ensg_in_data,]  
-  
-# 创建一个从ENSG号到基因符号的映射向量  
-ensg_to_symbol_vec <- setNames(as.character(ensg_to_symbol$symbol), ensg_to_symbol$ensembl_id)  
-rownames(DATA) <- ensg_to_symbol_vec[rownames(DATA)]
-
-
+DATA=DATA[USED,]
+rownames(DATA)=SYMBOL[USED]
 
